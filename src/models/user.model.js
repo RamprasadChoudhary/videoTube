0e5990757c88bf9e1f3b8b752d82c4e8e53a42ee
RAM PRASAD CHOUDHARY <ramranwa6@gmail.com>
2024-06-07 20:53:41 +0530

    JWT done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -66,4 +66,31 @@ userSchema.methods.isPasswordCorrect = async function
     return await bcrypt.compare(password,this.password)
 }
 
+
+userSchema.methods.generateAccessToken = function() {
+   jwt.sign(
+    {
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullName:this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+   ) 
+}
+userSchema.methods.generateRefreshToken = function() {
+    jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_tOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_tOKEN_EXPIRY
+        }
+       ) 
+}
+
 export const User = mongoose.model("User",userSchema)
